7a333cad921ad5d546503c4088b14095cdbb95eb
niel98 <mosesdaniel201798@gmail.com>
2020-08-27 09:01:58 +0000

    final commit

--- src/components/sideNav/SideNav.js
+++ src/components/sideNav/SideNav.js
@@ -4,21 +4,21 @@ import { Contact } from '../contact/Contact'
 
 export const SideNav = ({ handleSectionClick }) => {
     return (
-        <div className = 'sidenav'>
+        <div className='sidenav'>
             <img
-                src = 'https://pbs.twimg.com/profile_images/1278570628552015872/oLRRdZEO_400x400.jpg'
-                className = 'avatar'
-                alt = 'avatar'
-                />
-                <h2 className = 'main-name'> Moses Daniel Kwaknat </h2>
-                <p className = 'subtitle'> "Passionate about tech, Software developer and Data Analyst" </p>
-                <div className = 'sections-list'>
-                    <p className = 'section-list-element' onClick = {()=> handleSectionClick('about')}> About Me </p>
-                    <p className = 'section-list-element' onClick = {()=> handleSectionClick('skills')}> Skills </p>
-                    <p className = 'section-list-element' onClick = {()=> handleSectionClick('projects')}> Projects </p>
-                    <p className = 'section-list-element' onClick = {() => handleSectionClick('experiences')}> Experiences </p>  
-                </div>
-                <Contact />
+                src='https://avatars2.githubusercontent.com/u/41029959?s=460&u=0636ca51d286cdeaf1192a76215c5390eed79788&v=4'
+                className='avatar'
+                alt='avatar'
+            />
+            <h2 className='main-name'> Moses Daniel Kwaknat </h2>
+            <p className='subtitle'> "Passionate about tech, Software developer and Data Analyst" </p>
+            <div className='sections-list'>
+                <p className='section-list-element' onClick={() => handleSectionClick('about')}> About Me </p>
+                <p className='section-list-element' onClick={() => handleSectionClick('skills')}> Skills </p>
+                <p className='section-list-element' onClick={() => handleSectionClick('projects')}> Projects </p>
+                <p className='section-list-element' onClick={() => handleSectionClick('experiences')}> Experiences </p>
+            </div>
+            <Contact />
         </div>
     )
 }
